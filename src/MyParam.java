32e1b7004cf2f18f83236dfbb89ac5b96c61d128
KeebHub <94605914+KeebHub@users.noreply.github.com>
2022-06-13 23:45:03 +0800

    Added assignment help by Sham

---
+++ src/MyParam.java
@@ -0,0 +1,12 @@
+public class MyParam {
+
+    private static String name2;
+
+    public static String getName2() {
+        return name2;
+    }
+
+    public static void setName2(String n) {
+        name2 = n;
+    }
+}
